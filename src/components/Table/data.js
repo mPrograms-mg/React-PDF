f2d1d4f4fab072177c05fb81b3dde173db932a6c
Mahesh K <maheshkshirsagar128@gmail.com>
2024-05-29 19:50:46 +0530

    table data

---
+++ src/components/Table/data.js
@@ -0,0 +1,36 @@
+const invoiceTable = {
+    "items": [
+      {
+        "sno": 1,
+        "date":"30/05/2024",
+        "Amount": 5,
+        "status": 405.89
+      },
+      {
+        "sno": 2,
+        "date":"30/05/2024",
+        "Amount": 5,
+        "status": 373.11
+      },
+      {
+        "sno": 3,
+        "date":"30/05/2024",
+        "Amount": 5,
+        "status": 458.61
+      },
+      {
+        "sno": 4,
+        "date":"30/05/2024",
+        "Amount": 10,
+        "status": 725.24
+      },
+      {
+        "sno": 5,
+        "date":"30/05/2024",
+        "Amount": 4,
+        "status": 141.02
+      }
+    ]
+  }
+  
+  export default invoiceTable
